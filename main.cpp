aa23726a2d5babb48101e5b2428aeca2f4548f5c
bredenhulse <bredenhulse@gmail.com>
2021-09-29 08:36:10 -0500

    Changes

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+    //create stack
+    stack<char> conversion;
+    string postfix = "(((A-B)+C*D+E)/F))";
+    //iterate through string to look at every character
+    for(int i = 0; i < postfix.length(); i++){
+        if(postfix[i] == '('){
+            conversion.push('(');
+        }
+        else if(postfix[i] == '+'){
+            conversion.push('+');
+        }
+        else if(postfix[i] == '-'){
+            conversion.push('-');
+        }
+    }
+}
